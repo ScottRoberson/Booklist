2356a45408ac214fb89899190b29248d54914863
Scott Roberson <srkazan23@gmail.com>
2019-11-13 21:01:00 -0600

    Added book and UI class

---
+++ app.js
@@ -0,0 +1,40 @@
+//Book Constructor
+function Book(title,author,isbn){
+this.title = title;
+this.author = author;
+this.isbn = isbn;
+}
+
+//UI Constructor
+function UI(){}
+
+//Add Book to list
+UI.prototype.addBookToList = function(book){
+  console.log(book);
+}
+
+//Event Listeners
+document.getElementById('book-form').addEventListener('submit',function(e){
+  
+  //Get form values
+  const title = document.getElementById('title').value,
+  author =document.getElementById('author').value,
+  isbn =document.getElementById('isbn').value
+
+  //Instantiate book
+  const book = new Book(title,author,isbn);
+
+  //Instantiate UI
+  const ui = new UI()
+
+  //add book to list
+  ui.addBookToList(book);
+  
+
+  e.preventDefault();
+
+
+});
+
+
+
