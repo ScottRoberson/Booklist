0319d390e3f475b8bbdd172f2549df914b452b92
Scott Roberson <srkazan23@gmail.com>
2019-11-15 20:21:39 -0600

    finished addbook & clearfield prototype

--- app.js
+++ app.js
@@ -10,8 +10,26 @@ function UI(){}
 
 //Add Book to list
 UI.prototype.addBookToList = function(book){
-  console.log(book);
+  const list = document.getElementById('book-list');
+  //Create tr element
+  const row = document.createElement('tr');
+  //Insert Cols
+  row.innerHTML=`
+  <td>${book.title}</td>
+  <td>${book.author}</td>
+  <td>${book.isbn}</td>
+  <td><a href="#" class="delete">X</a></td>
+  `;
+  list.appendChild(row);
 }
+
+UI.prototype.clearFileds = function(){
+  document.getElementById('title').value = ''
+  document.getElementById('author').value = ''
+  document.getElementById('isbn').value = ''
+
+}
+
 
 //Event Listeners
 document.getElementById('book-form').addEventListener('submit',function(e){
@@ -26,10 +44,11 @@ document.getElementById('book-form').addEventListener('submit',function(e){
 
   //Instantiate UI
   const ui = new UI()
-
+  
   //add book to list
   ui.addBookToList(book);
   
+  ui.clearFileds();
 
   e.preventDefault();
 
